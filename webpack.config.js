88e2a78684b49e2d990832d44b09294f9680ba26
rodrigolamus <rodrigolamus@gmail.com>
2022-09-30 15:30:26 -0400

    project structure

---
+++ webpack.config.js
@@ -0,0 +1,48 @@
+const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
+
+module.exports = {
+    mode: 'development',
+    devtool: 'inline-source-map',
+    entry: './src/index.tsx',
+    output: {
+        path: path.join(__dirname, '/dist'),
+        filename: 'bundle.js'
+    },
+    devtool: 'inline-source-map',
+    devServer: {
+        static: './dist',
+    },
+    module: {
+        rules: [
+                {
+                test: /\.jsx?$/,
+                exclude: /node_modules/,
+                loader: 'babel-loader'
+            },
+            {
+                test: /\.tsx?$/,
+                exclude: /node_modules/,
+                loader: 'ts-loader'
+            },
+            {
+                test: /\.css$/,
+                exclude: /node_modules/,
+                use:['style-loader', 'css-loader']
+            },
+            {
+                test: /\.(jpe?g|png|gif|svg)$/i,
+                exclude: /node_modules/,
+                loader: 'file-loader'
+            }
+        ]
+    },
+    resolve: {
+    extensions: ['.tsx', '.ts', '.js'],
+    },
+    plugins:[
+        new HtmlWebpackPlugin({
+            template: './src/index.html'
+        })
+    ]
+}
